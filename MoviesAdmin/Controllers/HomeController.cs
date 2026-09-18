797d188ead1f2f5ea63e9cd9746ea183f5634c66
bakuncwa <gy.almirol@gmail.com>
2026-09-18 14:21:38 -0300

    Demo: Class Inheritance, Controller Routing

--- MoviesAdmin/Controllers/HomeController.cs
+++ MoviesAdmin/Controllers/HomeController.cs
@@ -6,20 +6,32 @@ namespace MoviesAdmin.Controllers
 {
     public class HomeController : Controller
     {
+        // Constructor
+        public HomeController()
+        {
+        }
+
+        //Action methods
+
+        // GET: Home/Index
         public IActionResult Index()
         {
+            Console.WriteLine("/Home/Index action method called");
             return View();
         }
 
+        // GET: Home/Privacy
         public IActionResult Privacy()
         {
+            Console.WriteLine("/Home/Privacy action method called");
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
     }
 }
---
+++ MoviesAdmin/Models/modelMovie.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAdmin.Models
+{
+    public class modelMovie
+    {
+        // Add a class for Model (id, name, description)
+        // Display and use in for-each loop
+            public int Id { get; set; }
+
+            [Required]
+            [StringLength(200)]
+            public string Title { get; set; } = string.Empty;
+
+            [StringLength(2000)]
+            public string? Synopsis { get; set; }
+        
+    }
+}
